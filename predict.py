744104d632cbd8149751dec602d98e823cee06e8
YooSungHyun <shyu0522@hanmail.net>
2023-08-28 17:39:29 +0900

    Update: 코드 유연성 개선 (utils폴더)

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 # for type annotation
 import os
-from argparse import Namespace
-from typing import Any, Dict
+from dataclasses import dataclass, field
+from typing import Dict, Literal
 
 import numpy as np
 import torch
@@ -13,14 +13,38 @@ from transformers import (
     DataCollatorWithPadding,
     HfArgumentParser,
     Trainer,
+    TrainingArguments,
     set_seed,
 )
 from transformers.trainer_utils import EvalPrediction
-from utils import clean_unicode, trim_text, PredictArguments
+from utils import clean_unicode, trim_text
 
 PASSAGE_QUESTION_TOKEN = "[SEP]"
 
 os.environ["TORCHDYNAMO_DISABLE"] = "1"
+
+
+@dataclass
+class PredictArguments(TrainingArguments):
+    model_name_or_path: str = field(
+        default=None,
+        metadata={"help": "predict에 사용할 모델의 이름 혹은 폴더 경로"},
+    )
+    data_file_path: str = field(
+        metadata={"help": "predict을 수행할 데이터 파일의 경로"},
+    )
+    data_fils_ext: Literal["json", "csv", "parquet", "text"] = field(
+        default="json",
+        metadata={"help": "불러들일 파일의 확장자"},
+    )
+    save_dir: str = field(
+        default="predict의 결과물을 저장할 폴더의 경로",
+        metadata={"help": ""},
+    )
+    output_dir: str = field(
+        default="./",
+        metadata={"help": "The output directory where the model predictions and checkpoints will be written."},
+    )
 
 
 def main(parser: HfArgumentParser) -> None:
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from arguments import PredictArguments
-from utils import clean_unicode, trim_text
